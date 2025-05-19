6f0752769b080ba4303a16f7942f31881bf4a223
lucastenani <lucas.tenani3@gmail.com>
2025-05-18 23:43:44 -0300

    refactor - apply SOLID dependency inversion when saving user with Prisma

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,6 @@
 import { hash } from 'bcryptjs'
 
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -9,20 +8,18 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists')
+    const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists')
+    }
+
+    await this.usersRepository.create({ name, email, password_hash })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  prismaUsersRepository.create({ name, email, password_hash })
 }
