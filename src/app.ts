7296a19d22c0b5c189c29a980b339068cfeb43f4
lucastenani <lucas.tenani3@gmail.com>
2025-05-20 15:49:01 -0300

    feat - implement global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
-import fastify from 'fastify'
+import fastify, { FastifyError, FastifyReply, FastifyRequest } from 'fastify'
+import { ZodError } from 'zod'
 
+import { env } from './env'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler(
+  (error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
+    if (error instanceof ZodError) {
+      return reply
+        .status(400)
+        .send({ message: 'Validation error', issues: error.format() })
+    }
+
+    if (env.NODE_ENV !== 'production') {
+      console.error(error)
+    } else {
+      // TODO: Here we should log to an external tool like datadog
+    }
+
+    return reply.status(500).send({ message: 'Internal server error' })
+  },
+)
