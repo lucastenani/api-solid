630e8fca1fa809a2c3069c207e7f6518bb3202df
lucastenani <lucas.tenani3@gmail.com>
2025-05-20 22:31:49 -0300

    refactor - decouple user registration unit test from database

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,14 +1,25 @@
 import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-
 import { RegisterUseCase } from './register'
 
 describe('Register use case', () => {
   it('should hash user password upon registration', async () => {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail() {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          created_at: new Date(),
+          password_hash: data.password_hash,
+        }
+      },
+    })
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
